0048beff1c14bc18cf95dcd7a47b43d2bfaea253
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-05-09 12:24:24 +0530

    Update : all

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.scss";
+import { FaBeer } from "react-icons/fa";
 
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <h1 className="bg-success">
+      <FaBeer />
+      Hello
+    </h1>
+  );
 }
 
 export default App;
